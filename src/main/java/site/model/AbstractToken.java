e7f59f7844bebea373478daca00d550f2b39bd3d
Ivona Mircheva <ivona1mircheva@gmail.com>
2018-07-11 14:39:36 +0300

    Add GDPRToken

---
+++ src/main/java/site/model/AbstractToken.java
@@ -0,0 +1,44 @@
+package site.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@MappedSuperclass
+public abstract class AbstractToken extends AbstractEntity {
+
+    private static final long serialVersionUID = -3626660176280247512L;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User owner;
+
+    @Column(unique = true)
+    @NotBlank
+    private String tokenId;
+
+    public AbstractToken() {}
+
+    public AbstractToken(User owner, String tokenId) {
+        this.owner = owner;
+        this.tokenId = tokenId;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getTokenId() {
+        return tokenId;
+    }
+
+    public void setTokenId(String tokenId) {
+        this.tokenId = tokenId;
+    }
+
+
+
+}
--- src/main/java/site/model/ResetPasswordToken.java
+++ src/main/java/site/model/ResetPasswordToken.java
@@ -1,75 +1,34 @@
 package site.model;
 
-import javax.persistence.Column;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 
-
-/**
- * @author Zhorzh Raychev
- */
 @Entity
 @Table(name = "ResetPasswordToken")
-public class ResetPasswordToken extends AbstractEntity {
+public class ResetPasswordToken extends AbstractToken {
 
-	private static final long serialVersionUID = -3626660176280247512L;
-	
-	@ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "userId", referencedColumnName = "id")
-	private User owner;
-	
-	@Column(unique = true)
-    @NotBlank
-	private String tokenId;
-	
 	private boolean used = false;
 
 	public ResetPasswordToken() {
-		
+		super();
 	}
-	
+
 	public ResetPasswordToken(User owner, String tokenId) {
-		this.owner = owner;
-		this.tokenId = tokenId;
+		super();
 	}
 
-	public User getOwner() {
-		return owner;
+	@Override
+	public String toString() {
+		return "ResetPasswordToken [owner=" + this.getOwner() + ", tokenId=" + this.getTokenId() + ", used=" + used + "]";
 	}
 
-	public void setOwner(User owner) {
-		this.owner = owner;
-	}
-
-	public String getTokenId() {
-		return tokenId;
-	}
-
-	public void setTokenId(String tokenId) {
-		this.tokenId = tokenId;
-	}
-	
 	public boolean isUsed() {
 		return used;
 	}
 
 	public void setUsed(boolean used) {
 		this.used = used;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((owner == null) ? 0 : owner.hashCode());
-		result = prime * result + ((tokenId == null) ? 0 : tokenId.hashCode());
-		result = prime * result + (used ? 1231 : 1237);
-		return result;
 	}
 
 	@Override
@@ -81,15 +40,15 @@ public class ResetPasswordToken extends AbstractEntity {
 		if (getClass() != obj.getClass())
 			return false;
 		ResetPasswordToken other = (ResetPasswordToken) obj;
-		if (owner == null) {
-			if (other.owner != null)
+		if (getOwner() == null) {
+			if (other.getOwner() != null)
 				return false;
-		} else if (!owner.equals(other.owner))
+		} else if (!getOwner().equals(other.getOwner()))
 			return false;
-		if (tokenId == null) {
-			if (other.tokenId != null)
+		if (getTokenId() == null) {
+			if (other.getTokenId() != null)
 				return false;
-		} else if (!tokenId.equals(other.tokenId))
+		} else if (!getTokenId().equals(other.getTokenId()))
 			return false;
 		if (used != other.used)
 			return false;
@@ -97,8 +56,14 @@ public class ResetPasswordToken extends AbstractEntity {
 	}
 
 	@Override
-	public String toString() {
-		return "ResetPasswordToken [owner=" + owner + ", tokenId=" + tokenId + ", used=" + used + "]";
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((getOwner() == null) ? 0 : getOwner().hashCode());
+		result = prime * result + ((getTokenId() == null) ? 0 : getTokenId().hashCode());
+		result = prime * result + (used ? 1231 : 1237);
+		return result;
 	}
-	
+
+
 }
